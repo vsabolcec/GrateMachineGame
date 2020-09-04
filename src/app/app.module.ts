26172881bb116d27d331f3caed89acfe8632ad1a
Vinko Sabolcec <vinko.sabolcec1@gmail.com>
2020-09-04 12:40:08 +0200

    add startmenu, credits and options components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { DominoComponent } from './domino/domino.component';
 import { BoardComponent } from './board/board.component';
 import { InventoryComponent } from './inventory/inventory.component';
+import { StartMenuComponent } from './start-menu/start-menu.component';
+import { OptionsMenuComponent } from './options-menu/options-menu.component';
+import { CreditsMenuComponent } from './credits-menu/credits-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DominoComponent,
     BoardComponent,
-    InventoryComponent
+    InventoryComponent,
+    StartMenuComponent,
+    OptionsMenuComponent,
+    CreditsMenuComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/credits-menu/credits-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-credits-menu',
+  templateUrl: './credits-menu.component.html',
+  styleUrls: ['./credits-menu.component.scss']
+})
+export class CreditsMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
