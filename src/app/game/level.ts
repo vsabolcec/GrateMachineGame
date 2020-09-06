ae863dd7064567c436435df9c9100d813fce9cc9
Vinko Sabolcec <vinko.sabolcec1@gmail.com>
2020-09-06 19:28:29 +0200

    Add more levels and narrative

--- src/app/game/level.ts
+++ src/app/game/level.ts
@@ -51,90 +51,12 @@ const defaultComplete =
 
 
 export const LEVELS: Level[] = [
-  // jos jedan + loop varijanta(treba popraviti broj straight pipeova, al ima bug tu)
-  {
-    startingBonus: { straightPipes: 12, turnPipes: 5, plusPipes: 1 },
-    messages: [],
-    blockedFields: [
-      '0000000000',
-      '0000010000',
-      '0000000000',
-      '0000010000',
-      '0000000000'
-    ],
-    grateMachines: [ { x: 5, y: 0 }, { x: 5, y: 4 }, { x: 3, y: 3 } ],
-    completed: defaultComplete
-  },
-  // ovaj bi se mogel nazvat difficult, al nisam provjeril jer ne radi algoritam :/
-  { 
-    startingBonus: { straightPipes: 7, turnPipes: 7 },
-    messages: [],
-    blockedFields: [
-      '0000001000',
-      '0000000000',
-      '0001001000',
-      '0000000000',
-      '0000000100'
-    ],
-    grateMachines: [ { x: 2, y: 1 } ,{ x: 6, y: 1 }, { x: 4, y: 2 }, { x: 6, y: 3} ],
-    completed: defaultComplete
-  },
-   // ovo je jedan laksi s plus pipeovima (isto trenutno ne radi zbog algoritma)
-   {
-    startingBonus: { straightPipes: 6, plusPipes: 2 },
-    messages: [],
-    blockedFields: [
-      '0000000000',
-      '0000000000',
-      '0000000000',
-      '0000000000',
-      '0000000000'
-    ],
-    grateMachines: [ { x: 3, y: 1 } ,{ x: 6, y: 1 }, { x: 3, y: 3 }, { x: 6, y: 3} ],
-    completed: defaultComplete
-  },
-  // jos jedan actually good level na slicnu nisu (ovaj trenutno ne radi zbog trenutnog algoritma u board.ts)
-  {
-    startingBonus: { straightPipes: 17, turnPipes: 4, plusPipes: 2 },
-    messages: [],
-    blockedFields: [
-      '0000000000',
-      '0000000000',
-      '0000000010',
-      '0000001000',
-      '0000001000'
-    ],
-    grateMachines: [ { x: 1, y: 0 }, { x: 5, y: 4 }, { x: 6, y: 2} ],
-    completed: defaultComplete
-  },
-   // jedan actually good level
-   {
-    startingBonus: { straightPipes: 10, turnPipes: 4, plusPipes: 1 },
-    messages: [],
-    blockedFields: [
-      '0000000000',
-      '0000000000',
-      '0000000000',
-      '0011100000',
-      '0000000000'
-    ],
-    grateMachines: [ { x: 1, y: 0 }, { x: 5, y: 4 } ],
-    completed: defaultComplete
-  },
- 
-  // + loop level
-  {
-    startingBonus: { straightPipes: 14, plusPipes: 1, turnPipes: 2 },
-    messages: [],
-    grateMachines: [ { x: 3, y: 0 }, { x: 6, y: 0 }, { x: 3, y: 4 }, { x: 6, y: 4 } ],
-    completed: defaultComplete
-  },
   // level 1
   {
-    startingBonus: { straightPipes: 8, plusPipes: 8, turnPipes: 8 },
+    startingBonus: { straightPipes: 8 },
     messages: [
       "Hi there! We're about to make a very great grate machine!",
-      "But first, we'll start with some infrastructure. I acquired 8 straight pipes - use them to make a connection between two parts of steam engine!"
+      "But first, we'll start with some infrastructure. I acquired 8 straight pipes - use them to make a connection between two steam machines!"
     ],
     completed: defaultComplete
   },
@@ -143,7 +65,7 @@ export const LEVELS: Level[] = [
     startingBonus: { straightPipes: 6, turnPipes: 4 },
     messages: [
       "Well done!",
-      "We'll continue with the infrastructure. I couldn't find much straight pipes this time, but I hope you can get use of these turn pipes as well!"
+      "We'll continue with the infrastructure. I couldn't find that much straight pipes this time, but I hope you can get use of these turn pipes as well!"
     ],
     completed: defaultComplete
   },
@@ -168,17 +90,157 @@ export const LEVELS: Level[] = [
     startingBonus: { straightPipes: 8, turnPipes: 3 },
     grateMachines: [ {x: 6, y: 0} ],
     messages: [
-      "OK, if I've spent all money on the pipes!",
-      "It's time to produce some grates finally! Connect the grate machine to both steam engine parts!"
+      "OK, I've spent all of the money on pipes!",
+      "It's time to produce some grates! Connect the grate machine to both steam engine parts!"
     ],
     completed: defaultComplete
   },
-  // game completed
+  // level 5
   {
+    startingBonus: { straightPipes: 7, turnPipes: 3 },
+    messages: [
+      "You can also connect multiple grate machines with each other so you gain more points.",
+      "Try it out!"
+    ],
+    blockedFields: [
+      '0000000000',
+      '0000000000',
+      '0000000000',
+      '0000000000',
+      '0000000000'
+    ],
+    grateMachines: [ { x: 3, y: 1 }, { x: 6, y: 3} ],
+    completed: defaultComplete
+  },
+  // level 6
+  {
+    startingBonus: { straightPipes: 6, turnPipes: 6 },
+    messages: [],
+    blockedFields: [
+      '0000000100',
+      '0010000000',
+      '0001000000',
+      '0000010001',
+      '0000000000'
+    ],
+    grateMachines: [ { x: 3, y: 1 }, { x: 6, y: 3} ],
+    completed: defaultComplete
+  },  
+  // level 7
+  {
+    startingBonus: { straightPipes: 9, turnPipes: 6 },
+    messages: [
+      "OK, I think you've gotten the hang of it. I am leaving you in charge of connecting these grate machines.",
+      "Try to connect all of them to make a very great grate machine.",
+      "Good luck!"
+    ],
+    blockedFields: [
+      '0010000000',
+      '0000000000',
+      '0000100000',
+      '0001000000',
+      '0000000000'
+    ],
+    grateMachines: [ { x: 1, y: 0 }, { x: 3, y: 4 }, { x: 3, y: 2 } ],
+    completed: defaultComplete
+  },
+   // level 8
+   {
+    startingBonus: { straightPipes: 10, turnPipes: 7 },
+    messages: [],
+    blockedFields: [
+      '1000100000',
+      '0000000000',
+      '0001100000',
+      '0000001000',
+      '0000000010'
+    ],
+    grateMachines: [ { x: 1, y: 0 }, { x: 7, y: 4 }, { x: 6, y: 2 } ],
+    completed: defaultComplete
+  },
+  // level 9
+  {
+    startingBonus: { straightPipes: 14, plusPipes: 1, turnPipes: 2 },
+    messages: [
+      "Our supplier has given us a new kind of pipes! A plus pipe!",
+      "These pipes can be used to achieve all kinds of new connections!",
+      "I assume that you can figure it out since you've gotten this far!"
+
+    ],
+    grateMachines: [ { x: 3, y: 0 }, { x: 6, y: 0 }, { x: 3, y: 4 }, { x: 6, y: 4 } ],
+    completed: defaultComplete
+  },
+  // level 10
+  { 
+    startingBonus: { straightPipes: 7, turnPipes: 7 },
+    messages: [
+      "You are really good at connecting these pipes!",
+      "Can you figure this one out?"
+    ],
+    blockedFields: [
+      '0000001000',
+      '0000000000',
+      '0001001000',
+      '0000000000',
+      '0000000100'
+    ],
+    grateMachines: [ { x: 2, y: 1 } ,{ x: 6, y: 1 }, { x: 4, y: 2 }, { x: 6, y: 3} ],
+    completed: defaultComplete
+  },
+  // level 11
+  {
+    startingBonus: { straightPipes: 17, turnPipes: 4, plusPipes: 2 },
+    messages: [
+      "Here is something a little more challenging for you!"
+    ],
+    blockedFields: [
+      '0000000000',
+      '0000000000',
+      '0000000010',
+      '0000001000',
+      '0000001000'
+    ],
+    grateMachines: [ { x: 1, y: 0 }, { x: 5, y: 4 }, { x: 6, y: 2} ],
+    completed: defaultComplete
+  },
+  // level 12
+  {
+    startingBonus: { straightPipes: 12, turnPipes: 5, plusPipes: 1 },
+    messages: [
+      "Damn!",
+      "You have almost completed our very great grate machine!",
+      "Keep up the good work!"
+    ],
+    blockedFields: [
+      '0000000000',
+      '0000010000',
+      '0000000000',
+      '0000010000',
+      '0000000000'
+    ],
+    grateMachines: [ { x: 5, y: 0 }, { x: 5, y: 4 }, { x: 3, y: 3 } ],
+    completed: defaultComplete
+  },
+   // level 13
+   {
+    startingBonus: { straightPipes: 10, turnPipes: 4, plusPipes: 1 },
+    messages: [],
+    blockedFields: [
+      '0000000000',
+      '0000010000',
+      '0000000000',
+      '0011100000',
+      '0000000000'
+    ],
+    grateMachines: [ { x: 1, y: 0 }, { x: 5, y: 4 } ],
+    completed: defaultComplete
+  },
+  // game completed
+   {
     startingBonus: { },
     messages: [
       "Seems like you have completed all of the levels!",
-      "Thank you for playing the game."
+      "Props to you for completing the game, you did really well!!"
     ],
     blockedFields: [
       '1111111111',
